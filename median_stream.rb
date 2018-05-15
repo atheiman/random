7e83bba66675fb3fb9db40a9fbb08031378b8058
Austin Heiman <atheimanksu@gmail.com>
2018-05-14 22:55:40 -0500

    format

--- median_stream.rb
+++ median_stream.rb
@@ -1,10 +1,13 @@
 # This problem was asked by Microsoft.
 
-# Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
+# Compute the running median of a sequence of numbers. That is, given a stream
+# of numbers, print out the median of the list so far on each new element.
 
-# Recall that the median of an even-numbered list is the average of the two middle numbers.
+# Recall that the median of an even-numbered list is the average of the two
+# middle numbers.
 
-# For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
+# For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should
+# print out:
 
 # 2
 # 1.5
